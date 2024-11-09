ee70d2014acdffb41b0445ea1979b8057b6b25ad
Rika-L <2209393334@qq.com>
2024-11-09 12:56:35 +0800

    chore: 更新依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
+import Rika from 'rika-plugin/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
@@ -15,20 +16,26 @@ export default defineConfig({
       '@/': `${path.resolve(__dirname, 'src')}/`,
     },
   },
-  plugins: [vue(), AutoImport({
-    imports: [
-      'vue',
-      '@vueuse/core',
-      'vitest',
-    ],
-    dts: 'src/auto-imports.d.ts',
-    vueTemplate: true,
-  }), Components({
-    extensions: ['vue'],
-    include: [/\.vue$/, /\.vue\?vue/],
-    dts: 'src/components.d.ts',
-  }), vueDevTools(
+  plugins: [
+    Rika(),
+    vue(),
+    AutoImport({
+      imports: [
+        'vue',
+        '@vueuse/core',
+        'vitest',
+      ],
+      dts: 'src/auto-imports.d.ts',
+      vueTemplate: true,
+    }),
+    Components({
+      extensions: ['vue'],
+      include: [/\.vue$/, /\.vue\?vue/],
+      dts: 'src/components.d.ts',
+    }),
+    vueDevTools(
     // 根据需求修改
-    { launchEditor: 'code' },
-  )],
+      { launchEditor: 'code' },
+    ),
+  ],
 })
